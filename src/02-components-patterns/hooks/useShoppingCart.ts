1fe151c216c3151f34935fcbdaf53ca2e6ae5f29
Kessler Torres <kesslertorres497@gmail.com>
2022-06-23 11:51:57 -0600

    components props alternative working

--- src/02-components-patterns/hooks/useShoppingCart.ts
+++ src/02-components-patterns/hooks/useShoppingCart.ts
@@ -8,30 +8,15 @@ export const useShoppingCart = () => {
 
         setShoppingCart( oldShoppingInCart => {
 
-            const productInCart: ProcuctInCart = oldShoppingInCart[product.id] || {...product, count: 0 };
-
-            if ( Math.max(productInCart.count + count, 0) > 0 ) {
-                productInCart.count += count;
-                return{
-                    ...oldShoppingInCart,
-                    [product.id]: productInCart
-                }
+            if ( count === 0) {
+                const { [product.id] : toDelete, ...rest } = oldShoppingInCart;
+                return rest
             }
 
-            // Borrar el producto
-            
-            const { [product.id]: toDelete, ...rest } = oldShoppingInCart;
-            return {...rest}
-
-            // if ( count === 0) {
-            //     const { [product.id] : toDelete, ...rest } = oldProductInCart;
-            //     return rest
-            // }
-
-            // return{
-            //     ...oldProductInCart,
-            //     [ product.id ]: { ...product, count }
-            // }
+            return{
+                ...oldShoppingInCart,
+                [ product.id ]: { ...product, count }
+            }
 
         })
     }
